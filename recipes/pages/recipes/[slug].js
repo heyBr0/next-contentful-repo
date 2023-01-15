973f8735a7112ba096a140207f71ea0f437b6952
heyBr0 <benert@live.de>
2023-01-15 12:40:15 +0100

    pages and slugs

--- recipes/pages/recipes/[slug].js
+++ recipes/pages/recipes/[slug].js
@@ -1,4 +1,6 @@
 import { createClient } from "contentful"
+import Image from "next/image"
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
 
 const client = createClient({
   space: process.env.CONTENTFUL_SPACE_ID,
@@ -36,10 +38,33 @@ export async function getStaticProps({ params }){
 
 
 export default function RecipeDetails({recipe}) {
-  console.log(recipe);
+  
+  const { featuredImage, title, cookingTime, ingredients, method} = recipe.fields
+
   return (
     <div>
-      Recipe Details
+      <div className="banner">
+        <Image 
+        src={"https:" + featuredImage.fields.file.url}
+        width={featuredImage.fields.file.details.image.width}
+        height={featuredImage.fields.file.details.image.height}
+        />
+        <h2>{title}</h2>
+      </div>
+
+      <div className="info">
+        <p>Take about {cookingTime} mins</p>
+        <h3> Ingredients: </h3>
+        {ingredients.map(ing => (
+          <span key={ing}>{ing}</span>
+        ))}
+      </div>
+
+      <div className="method">
+        <h3>Method:</h3>
+        <div>{documentToReactComponents(method)}</div>
+      </div>
+
     </div>
   )
 }
